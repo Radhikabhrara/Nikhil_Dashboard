bd34efa326471a11521e95329f08006a474d76ca
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-07-27 23:44:17 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Adding Image to web app
+st.set_page_config(page_title="Nik", page_icon = im ,layout="wide",initial_sidebar_state="auto")
+st.title("Nikhil_Dashboard)
+
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
