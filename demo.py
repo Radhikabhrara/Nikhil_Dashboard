45fb6f4aab2d872f755e9705857a93be2ce06bc9
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-07-27 23:58:23 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Adding Image to web app
-st.set_page_config(page_title="Nik", page_icon = im ,layout="wide",initial_sidebar_state="auto")
+st.set_page_config(page_title="Nik",layout="wide",initial_sidebar_state="auto")
 st.title("Nikhil_Dashboard")
 
 hide_default_format = """
@@ -12,5 +12,33 @@ hide_default_format = """
        </style>
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
+
+'''
+
+def create_connection():
+    connection = psycopg2.connect(
+        host="your_host",
+        database="your_database",
+        user="your_username",
+        password="your_password"
+    )
+    return connection
+def fetch_data():
+    connection = create_connection()
+    query = "SELECT * FROM your_table;"
+    data = pd.read_sql(query, connection)
+    connection.close()
+    return data
+
+def update_data(data):
+    connection = create_connection()
+    with connection.cursor() as cursor:
+        for index, row in data.iterrows():
+            query = f"UPDATE your_table SET column1 = '{row['column1']}', column2 = '{row['column2']}' WHERE id = {row['id']};"
+            cursor.execute(query)
+        connection.commit()
+    connection.close()
+
+    '''
 
 
