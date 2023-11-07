8bada8612a93e33a5ce0c9b9215d6124ed492577
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-07 16:15:13 +0530

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pymysql
+
+# Establish a connection to your MySQL database
+def create_connection():
+    conn = pymysql.connect(
+        host='new-db-1.advasmart.in',
+        user='radhika-ro',
+        password='sYkcHssQBbUwIuJ',
+        db='advasmartdb'
+    )
+    return conn
+
+# Streamlit App
+st.title('MySQL Database Dashboard')
+
+# Connect to the database
+conn = create_connection()
+
+# Define a function to execute SQL queries
+def run_query(query):
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchall()
+    return result
+
+# Example: Display a table from your database
+st.header('Sample Table from the Database')
+query = "SELECT * FROM your_table_name"
+table_data = run_query(query)
+
+if table_data:
+    st.write(table_data)
+else:
+    st.write('No data to display.')
+
+# Close the database connection
+conn.close()
+
