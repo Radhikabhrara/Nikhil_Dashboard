c59802d5cb3102a0d863c45ec57a09c54c453d72
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-07 19:56:40 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,31 +12,36 @@ def create_connection():
     )
     return conn
 
+
 # Streamlit App
 st.title('MySQL Database Dashboard')
 
 # Connect to the database
 conn = create_connection()
 
-# Define a function to execute SQL queries
-def run_query(query):
+# Fetch column names from the database
+def get_column_names(table_name):
     with conn.cursor() as cursor:
-        cursor.execute(query)
-        result = cursor.fetchall()
-    return result
+        cursor.execute(f"DESCRIBE {table_name}")
+        columns = [row[0] for row in cursor.fetchall()]
+    return columns
 
+# Example: Display a table from your database
 st.header('Sample Table from the Database')
-
-query = "SELECT * FROM aggregate_daily_stats"
-table_data = run_query(query)
+table_name = 'aggregate_daily_stats'  # Replace with the table name you want to display
+columns = get_column_names(table_name)
 
 # Display the table data with column names as headers
-if table_data:
-    st.table([column[0] for column in cursor.description])  # Display column names
-    st.table(table_data)  # Display data
+if columns:
+    st.table(columns)  # Display column names as headers
+    query = f"SELECT * FROM {table_name}"
+    table_data = run_query(query)
+    if table_data:
+        st.table(table_data)  # Display data
+    else:
+        st.warning('No data available for the selected table.')
 else:
-    st.warning('No data available for the selected table.')
+    st.warning('No columns available for the selected table.')
 
 # Close the database connection
 conn.close()
-
