6c92171366e090d36c3c1836c072909e9d21c8ca
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-08 13:58:30 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,15 +19,15 @@ def create_connection():
     return connection
 
 # Function to fetch application data from the MySQL database
-def fetch_application_data(start_date, end_date, client, connection):
+def fetch_application_data(start_date, end_date, connection):
     try:
         with connection.cursor() as cursor:
             query = """
                 SELECT client_name, stat_date, comp_app_count, approv_app_count, yet_to_create_app_count, rejected_app_count
                 FROM aggregate_daily_stats_as_on
-                WHERE stat_date BETWEEN %s AND %s AND client_name = %s
+                WHERE stat_date BETWEEN %s AND %s
             """
-            cursor.execute(query, (start_date, end_date, client))
+            cursor.execute(query, (start_date, end_date))
             data = cursor.fetchall()
         return data
     except Exception as e:
@@ -35,15 +35,15 @@ def fetch_application_data(start_date, end_date, client, connection):
         return []
 
 # Function to fetch order data from the MySQL database
-def fetch_order_data(start_date, end_date, client, connection):
+def fetch_order_data(start_date, end_date, connection):
     try:
         with connection.cursor() as cursor:
             query = """
                 SELECT client_name, stat_date, man_order_count, auto_order_count, remain_order_count, total_order_count
                 FROM aggregate_daily_stats_as_on
-                WHERE stat_date BETWEEN %s AND %s AND client_name = %s
+                WHERE stat_date BETWEEN %s AND %s
             """
-            cursor.execute(query, (start_date, end_date, client))
+            cursor.execute(query, (start_date, end_date))
             data = cursor.fetchall()
         return data
     except Exception as e:
@@ -57,16 +57,10 @@ st.title("MySQL Database Dashboard")
 conn = create_connection()
 
 if conn is not None:
-    # Select data level (application, order, or both)
-    data_level = st.radio("Select Data Level:", ["Application Level", "Order Level", "Both"])
-
     # Date Range Filter
     st.sidebar.write("### Date Range Filter")
     start_date = st.sidebar.date_input("Start Date")
     end_date = st.sidebar.date_input("End Date")
-
-    # Client Selection
-    client = st.sidebar.selectbox("Select Client:", ["Client A", "Client B", "Client C"])
 
     # Default date range for initial data display
     if not start_date:
@@ -74,39 +68,49 @@ if conn is not None:
     if not end_date:
         end_date = pd.to_datetime("2023-12-31")
 
-    if data_level in ["Application Level", "Both"]:
-        # Example: Display a table from your database - Application Level
-        st.header('Application Level Data')
-        application_data = fetch_application_data(start_date, end_date, client, conn)
+    # Example: Display a table from your database - Application Level
+    st.header('Application Level Data')
+    application_data = fetch_application_data(start_date, end_date, conn)
 
-        # Create a DataFrame for application data
-        df_app = pd.DataFrame(application_data, columns=["Client Name", "Date", "Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"])
+    # Create a DataFrame for application data
+    df_app = pd.DataFrame(application_data, columns=["Client Name", "Date", "Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"])
 
-        # Display the application data
-        st.write("### Application Count Data")
-        st.dataframe(df_app)
+    # Display the application data
+    st.write("### Application Count Data")
+    st.dataframe(df_app)
 
-        # Interactive Bar Chart for Application Level
-        st.write("### Application level Bar Chart")
-        fig_app = px.bar(df_app, x="Client Name", y=["Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"], title="Application Count")
-        st.plotly_chart(fig_app)
+    # Interactive Bar Chart for Application Level
+    st.write("### Application level Bar Chart")
+    fig_app = px.bar(df_app, x="Client Name", y=["Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"], title="Application Count")
+    st.plotly_chart(fig_app)
 
-    if data_level in ["Order Level", "Both"]:
-        # Example: Display a table from your database - Order Level
-        st.header('Order Level Data')
-        order_data = fetch_order_data(start_date, end_date, client, conn)
+    # Create a pie chart using Plotly Express for Application Level
+    st.write("### Application level Pie Chart")
+    df_app['Values'] = df_app[['Completed Application', 'Approved Applications', 'Yet to Create Applications', 'Rejected Applications']].sum(axis=1)
+    fig_app_pie = px.pie(df_app, names="Client Name", values="Values", title="Application Count")
+    st.plotly_chart(fig_app_pie)
 
-        # Create a DataFrame for order data
-        df_order = pd.DataFrame(order_data, columns=["Client Name", "Date", "Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"])
+    # Example: Display a table from your database - Order Level
+    st.header('Order Level Data')
+    order_data = fetch_order_data(start_date, end_date, conn)
 
-        # Display the order data
-        st.write("### Order Count Data")
-        st.dataframe(df_order)
+    # Create a DataFrame for order data
+    df_order = pd.DataFrame(order_data, columns=["Client Name", "Date", "Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"])
 
-        # Interactive Bar Chart for Order Level
-        st.write("### Order level Bar Chart")
-        fig_order = px.bar(df_order, x="Client Name", y=["Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"], title="Order Count")
-        st.plotly_chart(fig_order)
+    # Display the order data
+    st.write("### Order Count Data")
+    st.dataframe(df_order)
+
+    # Interactive Bar Chart for Order Level
+    st.write("### Order level Bar Chart")
+    fig_order = px.bar(df_order, x="Client Name", y=["Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"], title="Order Count")
+    st.plotly_chart(fig_order)
+
+    # Create a pie chart using Plotly Express for Order Level
+    st.write("### Order level Pie Chart")
+    df_order['Values'] = df_order[['Manual Orders', 'Auto Orders', 'Remaining Orders', 'Total Orders']].sum(axis=1)
+    fig_order_pie = px.pie(df_order, names="Client Name", values="Values", title="Order Count")
+    st.plotly_chart(fig_order_pie)
 
     # Close the database connection
     conn.close()
