7a4dd24022b17ba7b9958cb71bec8fb820503c99
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-08 14:01:10 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,9 @@
+
 import streamlit as st
 import pymysql
 import pandas as pd
 import plotly.express as px
+
 
 # Function to connect to the MySQL database
 def create_connection():
@@ -17,6 +19,7 @@ def create_connection():
     except Exception as e:
         st.error(f"Error: Unable to connect to the database. {e}")
     return connection
+
 
 # Function to fetch application data from the MySQL database
 def fetch_application_data(start_date, end_date, connection):
@@ -68,51 +71,63 @@ if conn is not None:
     if not end_date:
         end_date = pd.to_datetime("2023-12-31")
 
-    # Example: Display a table from your database - Application Level
-    st.header('Application Level Data')
-    application_data = fetch_application_data(start_date, end_date, conn)
+    # Checkbox to select data level (application, order, or both)
+    data_level_selection = st.sidebar.checkbox("Select Data Level")
+    application_level = False
+    order_level = False
 
-    # Create a DataFrame for application data
-    df_app = pd.DataFrame(application_data, columns=["Client Name", "Date", "Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"])
+    if data_level_selection:
+        application_level = st.sidebar.checkbox("Application Level", value=True)
+        order_level = st.sidebar.checkbox("Order Level", value=True)
 
-    # Display the application data
-    st.write("### Application Count Data")
-    st.dataframe(df_app)
+    if application_level:
+        # Example: Display a table from your database - Application Level
+        st.header('Application Level Data')
+        application_data = fetch_application_data(start_date, end_date, conn)
 
-    # Interactive Bar Chart for Application Level
-    st.write("### Application level Bar Chart")
-    fig_app = px.bar(df_app, x="Client Name", y=["Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"], title="Application Count")
-    st.plotly_chart(fig_app)
+        # Create a DataFrame for application data
+        df_app = pd.DataFrame(application_data, columns=["Client Name", "Date", "Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"])
 
-    # Create a pie chart using Plotly Express for Application Level
-    st.write("### Application level Pie Chart")
-    df_app['Values'] = df_app[['Completed Application', 'Approved Applications', 'Yet to Create Applications', 'Rejected Applications']].sum(axis=1)
-    fig_app_pie = px.pie(df_app, names="Client Name", values="Values", title="Application Count")
-    st.plotly_chart(fig_app_pie)
+        # Display the application data
+        st.write("### Application Count Data")
+        st.dataframe(df_app)
 
-    # Example: Display a table from your database - Order Level
-    st.header('Order Level Data')
-    order_data = fetch_order_data(start_date, end_date, conn)
+        # Interactive Bar Chart for Application Level
+        st.write("### Application level Bar Chart")
+        fig_app = px.bar(df_app, x="Client Name", y=["Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"], title="Application Count")
+        st.plotly_chart(fig_app)
 
-    # Create a DataFrame for order data
-    df_order = pd.DataFrame(order_data, columns=["Client Name", "Date", "Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"])
+        # Create a pie chart using Plotly Express for Application Level
+        st.write("### Application level Pie Chart")
+        df_app['Values'] = df_app[['Completed Application', 'Approved Applications', 'Yet to Create Applications', 'Rejected Applications']].sum(axis=1)
+        fig_app_pie = px.pie(df_app, names="Client Name", values="Values", title="Application Count")
+        st.plotly_chart(fig_app_pie)
 
-    # Display the order data
-    st.write("### Order Count Data")
-    st.dataframe(df_order)
+    if order_level:
+        # Example: Display a table from your database - Order Level
+        st.header('Order Level Data')
+        order_data = fetch_order_data(start_date, end_date, conn)
 
-    # Interactive Bar Chart for Order Level
-    st.write("### Order level Bar Chart")
-    fig_order = px.bar(df_order, x="Client Name", y=["Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"], title="Order Count")
-    st.plotly_chart(fig_order)
+        # Create a DataFrame for order data
+        df_order = pd.DataFrame(order_data, columns=["Client Name", "Date", "Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"])
 
-    # Create a pie chart using Plotly Express for Order Level
-    st.write("### Order level Pie Chart")
-    df_order['Values'] = df_order[['Manual Orders', 'Auto Orders', 'Remaining Orders', 'Total Orders']].sum(axis=1)
-    fig_order_pie = px.pie(df_order, names="Client Name", values="Values", title="Order Count")
-    st.plotly_chart(fig_order_pie)
+        # Display the order data
+        st.write("### Order Count Data")
+        st.dataframe(df_order)
+
+        # Interactive Bar Chart for Order Level
+        st.write("### Order level Bar Chart")
+        fig_order = px.bar(df_order, x="Client Name", y=["Manual Orders", "Auto Orders", "Remaining Orders", "Total Orders"], title="Order Count")
+        st.plotly_chart(fig_order)
+
+        # Create a pie chart using Plotly Express for Order Level
+        st.write("### Order level Pie Chart")
+        df_order['Values'] = df_order[['Manual Orders', 'Auto Orders', 'Remaining Orders', 'Total Orders']].sum(axis=1)
+        fig_order_pie = px.pie(df_order, names="Client Name", values="Values", title="Order Count")
+        st.plotly_chart(fig_order_pie)
 
     # Close the database connection
     conn.close()
 else:
     st.error("Unable to connect to the database.")
+
