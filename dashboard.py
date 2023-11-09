ff9a9402883bf1b5504ee87a28cb45b2a6fc554b
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-09 18:46:50 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -121,14 +121,15 @@ def fetch_comparison_data(start_date, end_date, data_level, time_frame, selected
 
             if data_level == "Application":
                 query = f"""
-              SELECT stat_date,client_name,
-                   SUM(comp_app_count) as total_comp_app_count, 
-                   SUM(approv_app_count) as total_approved_app_count,
-                   SUM(yet_to_create_app_count) as total_yet_to_create_app_count,
-                   SUM(rejected_app_count) as total_rejected_app_count
-              FROM aggregate_daily_stats_as_on
-              WHERE stat_date BETWEEN %s AND %s
-              GROUP BY stat_date
+                     SELECT stat_date,
+                            client_name,
+                            SUM(comp_app_count) as total_comp_app_count, 
+                            SUM(approv_app_count) as total_approved_app_count,
+                            SUM(yet_to_create_app_count) as total_yet_to_create_app_count,
+                            SUM(rejected_app_count) as total_rejected_app_count
+                      FROM aggregate_daily_stats_as_on
+                      WHERE stat_date BETWEEN %s AND %s
+                      GROUP BY stat_date, client_name;
                 """
             elif data_level == "Order":
                 query = f"""
