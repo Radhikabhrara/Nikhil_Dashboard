0b022af525567adabc30f01d487867437e30b03a
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-09 12:37:37 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,113 +1,107 @@
-# Import necessary libraries
-import pandas as pd
 import streamlit as st
+import pymysql
+import pandas as pd
 import plotly.express as px
-from datetime import timedelta
-from dateutil.relativedelta import relativedelta
 
-def fetch_data(start_date, end_date, connection, data_level):
+
+st.set_page_config(page_title="AdvaInsight",initial_sidebar_state="auto")
+# Add a logo and title using HTML
+
+
+
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+# Function to connect to the MySQL database
+def create_connection():
+    connection = None
+    try:
+        connection = pymysql.connect(
+            host='new-db-1.advasmart.in',
+            user='radhika-ro',
+            password='sYkcHssQBbUwIuJ',
+            port=3366,
+            db='advasmartdb'
+        )
+    except Exception as e:
+        st.error(f"Error: Unable to connect to the database. {e}")
+    return connection
+
+
+# Function to fetch application data from the MySQL database
+def fetch_application_data(start_date, end_date, connection):
     try:
         with connection.cursor() as cursor:
-            query = f"""
-                SELECT client_name, stat_date, 
-                {"comp_app_count, approv_app_count, yet_to_create_app_count, rejected_app_count" if data_level == "Application" else "man_order_count, auto_order_count, remain_order_count, total_order_count"}
+            query = """
+                SELECT client_name, stat_date, comp_app_count, approv_app_count, yet_to_create_app_count, rejected_app_count
                 FROM aggregate_daily_stats_as_on
                 WHERE stat_date BETWEEN %s AND %s
             """
-            print("Generated Query:", query)  # Add this line for debugging
             cursor.execute(query, (start_date, end_date))
             data = cursor.fetchall()
         return data
     except Exception as e:
-        st.error(f"Error: Unable to fetch data from the database. {e}")
+        st.error(f"Error: Unable to fetch application data from the database. {e}")
         return []
 
-
-
-# Function to generate reports
-def generate_reports(data, data_levels, time_period):
-    dfs = []
-
-    for data_level in data_levels:
-        df = pd.DataFrame(data, columns=["Client Name", "Date", data_level])
-
-        if time_period == "Weekly":
-            df['Week Start'] = df['Date'] - pd.to_timedelta(df['Date'].dt.dayofweek, unit='d')
-            df['Week End'] = df['Week Start'] + pd.DateOffset(days=6)
-
-            # Group by client and week, summing the values
-            grouped_df = df.groupby(['Client Name', 'Week Start', 'Week End']).sum().reset_index()
-
-        elif time_period == "Monthly":
-            df['Month'] = df['Date'].dt.to_period('M')
-
-            # Group by client and month, summing the values
-            grouped_df = df.groupby(['Client Name', 'Month']).sum().reset_index()
-
-        elif time_period == "Quarterly":
-            df['Quarter'] = df['Date'].dt.to_period('Q')
-
-            # Group by client and quarter, summing the values
-            grouped_df = df.groupby(['Client Name', 'Quarter']).sum().reset_index()
-
-        dfs.append(grouped_df)
-
-    return dfs
+# Function to fetch order data from the MySQL database
+def fetch_order_data(start_date, end_date, connection):
+    try:
+        with connection.cursor() as cursor:
+            query = """
+                SELECT client_name, stat_date, man_order_count, auto_order_count, remain_order_count, total_order_count
+                FROM aggregate_daily_stats_as_on
+                WHERE stat_date BETWEEN %s AND %s
+            """
+            cursor.execute(query, (start_date, end_date))
+            data = cursor.fetchall()
+        return data
+    except Exception as e:
+        st.error(f"Error: Unable to fetch order data from the database. {e}")
+        return []
 
 # Create a Streamlit app
-st.title("MySQL Database Dashboard")
+st.title("AdvaInsights")
 
 # Connect to the MySQL database
 conn = create_connection()
 
 if conn is not None:
-    st.sidebar.title("AdvaInsights")
+    # Navigation menu
+    page = st.sidebar.radio("Navigation", ["Customized Insights", "Generate Reports"])
 
-    st.sidebar.write("### Data Level and Report Generation")
+    if page == "Customized Insights":
+        st.sidebar.title("AdvaInsights")
 
-    # Dropdown to select data level (application, order, or both)
-    data_level = st.sidebar.selectbox("Select Data Level", ["Application", "Order", "Both"])
+        # Dropdown to select data level (application, order, or both)
+        data_level = st.sidebar.selectbox("Select Data Level", ["", "Application", "Order", "Both"])
 
-    # Date Range Filter
-    start_date = st.sidebar.date_input("Start Date", pd.to_datetime("2023-01-01"))
-    end_date = st.sidebar.date_input("End Date", pd.to_datetime("2023-12-31"))
+        st.sidebar.write("### Date Range Filter")
 
-    # Checkbox to filter data
-    filter_data = st.sidebar.checkbox("Filter Data")
+        # Date Range Filter
+        start_date = st.sidebar.date_input("Start Date")
+        end_date = st.sidebar.date_input("End Date")
 
-    # Multiselect to choose data levels for report generation
-    data_levels_for_report = st.sidebar.multiselect("Select Data Levels for Report", ["Application", "Order"])
+        # Default date range for initial data display
+        if not start_date:
+            start_date = pd.to_datetime("2023-01-01")
+        if not end_date:
+            end_date = pd.to_datetime("2023-12-31")
 
-    if st.sidebar.button("Generate Reports"):
-        st.title("Generate Reports")
+        st.sidebar.write("### Client Wise Filter")
+        # Checkbox to filter data
+        filter_data = st.sidebar.checkbox("Filter Data")
 
-        # Select time period for the report
-        time_period_report = st.selectbox("Select Time Period", ["Weekly", "Monthly", "Quarterly"])
-
-        # Fetch data based on selected parameters
-        report_data = fetch_data(start_date, end_date, conn, data_level)
-
-        # Generate the report
-        if report_data:
-            generated_reports = generate_reports(report_data, data_levels_for_report, time_period_report)
-
-            for i, data_level_report in enumerate(data_levels_for_report):
-                # Display the generated report
-                st.write(f"### Generated {data_level_report} Report ({time_period_report} Trend)")
-                st.dataframe(generated_reports[i])
-
-                # Interactive Bar Chart for the generated report
-                st.write(f"### {data_level_report} {time_period_report} Bar Chart")
-                fig_report = px.bar(generated_reports[i], x="Client Name", y=data_level_report.lower(), color=time_period_report, title=f"{data_level_report} Count")
-                st.plotly_chart(fig_report)
-
-    # Display data based on selected data level
-    if data_level in ["Application", "Both"]:
-        st.header('Application Level Data')
-        application_data = fetch_application_data(start_date, end_date, conn)
-
-        # Create a DataFrame for application data
-        df_app = pd.DataFrame(application_data, columns=["Client Name", "Date", "Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"])
+        if data_level == "Application" or data_level == "Both":
+            st.header('Application Level Data')
+            application_data = fetch_application_data(start_date, end_date, conn)
+            # Create a DataFrame for application data
+            df_app = pd.DataFrame(application_data, columns=["Client Name", "Date", "Completed Application", "Approved Applications", "Yet to Create Applications", "Rejected Applications"])
 
         if filter_data:
             # Filter data based on the checkbox
@@ -129,7 +123,8 @@ if conn is not None:
         fig_app_pie = px.pie(df_app, names="Client Name", values="Values", title="Application Count")
         st.plotly_chart(fig_app_pie)
 
-    if data_level in ["Order", "Both"]:
+    if data_level == "Order" or data_level == "Both":
+        # Example: Display a table from your database - Order Level
         st.header('Order Level Data')
         order_data = fetch_order_data(start_date, end_date, conn)
 
@@ -153,4 +148,16 @@ if conn is not None:
         # Create a pie chart using Plotly Express for Order Level
         st.write("### Order level Pie Chart")
         df_order['Values'] = df_order[['Manual Orders', 'Auto Orders', 'Remaining Orders', 'Total Orders']].sum(axis=1)
-        fig_order_pie = px.pie(df_order, names="Client Name",
+        fig_order_pie = px.pie(df_order, names="Client Name", values="Values", title="Order Count")
+        st.plotly_chart(fig_order_pie)
+
+
+    elif page == "Generate Reports":
+        # Placeholder for the "Generate Reports" page
+        st.title("Generate Reports Page")
+        st.write("This page is under construction.")
+
+    # Close the database connection
+    # conn.close()
+else:
+    st.error("Unable to connect to the database.")
