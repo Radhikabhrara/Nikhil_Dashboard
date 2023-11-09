ae98daed5a46e2817b1a63f93941346c3ebc90ce
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-11-09 18:55:03 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -371,7 +371,7 @@ if conn is not None:
 
         # Define column names based on the selected data level
         if selected_data_level == "Application":
-            columns = ["Stat Date", "Total Completed Applications", "Total Approved Applications", "Total Yet to Create Applications", "Total Rejected Applications"]
+            columns = ["Stat Date", "Client Name", "Total Completed Applications", "Total Approved Applications", "Total Yet to Create Applications", "Total Rejected Applications"]
         elif selected_data_level == "Order":
             columns = ["Stat Date", "Total Manual Orders", "Total Auto Orders", "Total Remaining Orders", "Total Total Orders"]
         elif selected_data_level == "API":
@@ -393,7 +393,7 @@ if conn is not None:
 
         # Interactive Line Chart for Comparison
         st.write(f"### {selected_time_frame} Comparison")
-        fig_comparison = px.line(df_comparison, x="Stat Date", y=columns[1:], title=f"{selected_data_level} {selected_time_frame} Comparison")
+        fig_comparison = px.line(df_comparison, x="Stat Date", y=columns[2:], title=f"{selected_data_level} {selected_time_frame} Comparison")
         st.plotly_chart(fig_comparison)
 
     # Close the database connection
